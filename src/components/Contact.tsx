fd0d443a10334c91ddfc653147ba1ecdfb3cfe3f
Arie Shapiro <aryehshapiro@gmail.com>
2019-01-08 14:37:04 +0100

    first commit

---
+++ src/components/Contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div className="container">
+          <h4 className="center">Contact</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia voluptatibus, eius suscipit repudiandae culpa cupiditate quidem veritatis, vitae corporis sapiente sequi maxime voluptatem ut, in velit eaque qui ipsam expedita.</p>
+        </div>
+    )
+}
+
+export default Contact;
---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="container">
+          <h4 className="center">Home</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia voluptatibus, eius suscipit repudiandae culpa cupiditate quidem veritatis, vitae corporis sapiente sequi maxime voluptatem ut, in velit eaque qui ipsam expedita.</p>
+        </div>
+    )
+}
+
+export default Home;
